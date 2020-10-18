7f764d8ebedcde8620f6563c74ecf473ac080364
Pierre Laclaverie <pierre.laclaverie@gmail.com>
2020-10-18 19:26:17 +0200

    fin activité 1
    c'est fait !

--- Activity2/dist/main.js
+++ Activity2/dist/main.js
@@ -1,1 +1,26 @@
-new Vue({el:"#app",data:{message:"Hello Vue!"}});
+
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+var app = new Vue({
+    el: '#app2',
+    data: {
+      message: 'Hello Vue.js !',
+      counter :0
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join(''),
+        this.counter+=1
+      }
+    }
+  })
+  var comptage = new Vue({
+      el:'#compteur',
+      data:{
+          counter:0
+      }
+  })
--- Activity2/src/index.js
+++ Activity2/src/index.js
@@ -6,3 +6,16 @@ var app = new Vue({
         message: 'Hello Vue!'
     }
 });
+var app = new Vue({
+    el: '#app2',
+    data: {
+      message: 'Hello Vue.js !',
+      counter :0
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join(''),
+        this.counter+=1
+      }
+    }
+  })
